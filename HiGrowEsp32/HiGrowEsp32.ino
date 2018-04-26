6a14f070893cd6f509aff3b8ea32f26c5fec49cc
LilyGO <31160602+LilyGO@users.noreply.github.com>
2018-04-26 14:02:54 +0800

    Update HiGrowEsp32.ino

--- HiGrowEsp32/HiGrowEsp32.ino
+++ HiGrowEsp32/HiGrowEsp32.ino
@@ -1,186 +1,175 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiServer.h>
-#include <HTTPClient.h>
-#include <Preferences.h>
-#include <SimpleDHT.h>
+/*
+   Rui Santos
+   Complete Project Details http://randomnerdtutorials.com
+*/
 
-Preferences preferences;
+#include <WiFi.h>
+#include "DHT.h"
+
+// Uncomment one of the lines below for whatever DHT sensor type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+#define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
+
+// Replace with your network credentials
+const char* ssid     = "";  //wifi id
+const char* password = "";  //password
 
 WiFiServer server(80);
 
-SimpleDHT11 dht11;
+// DHT Sensor
+const int DHTPin = 22;
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+// Temporary variables
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
 
-HTTPClient http;
-
-uint64_t chipid;
-
-long timeout;
-
-char deviceid[21];
+// Client variables
+char linebuf[80];
+int charcount = 0;
 
 void setup() {
+  // initialize the DHT sensor
+  dht.begin();
+
+  //Initialize serial and wait for port to open:
   Serial.begin(115200);
-
-  preferences.begin("higrow", false);
-
-  pinMode(16, OUTPUT); 
-  pinMode(0, INPUT);
-  digitalWrite(16, HIGH);
-
-  for (int j = 0; j < 30; j++) {
-    if (digitalRead(0) == LOW) {
-      preferences.clear();
-      digitalWrite(16, LOW);
-      delay(100);
-      digitalWrite(16, HIGH);
-      delay(100);
-      digitalWrite(16, LOW);
-      delay(100);
-      digitalWrite(16, HIGH);
-      j=30;
-    }
-    delay(100);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  timeout = 0;
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-  chipid = ESP.getEfuseMac();
-  sprintf(deviceid, "%" PRIu64, chipid);
-  Serial.println(deviceid);
+  WiFi.begin(ssid, password);
 
-  String ssid = preferences.getString("ssid");
-  String pwd = preferences.getString("pwd");
-
-  if (ssid.length() > 0 && pwd.length() > 0) {
-    // We start by connecting to a WiFi network
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid.c_str(), pwd.c_str());
-
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-
-    digitalWrite(16, HIGH);
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-  } else {
-    //Init WiFi as Station, start SmartConfig
-    WiFi.mode(WIFI_AP_STA);
-    WiFi.beginSmartConfig();
-
-    //Wait for SmartConfig packet from mobile
-    Serial.println("Waiting for SmartConfig.");
-    while (!WiFi.smartConfigDone()) {
-      int led = digitalRead(16);
-      if (led == HIGH) {
-        digitalWrite(16, LOW);
-      } else {
-        digitalWrite(16, HIGH);
-      }
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("SmartConfig received.");
-
-    //Wait for WiFi to connect to AP
-    Serial.println("Waiting for WiFi");
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi Connected.");
-
-    preferences.putString("ssid", WiFi.SSID());
-    preferences.putString("pwd", WiFi.psk());
-
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    delay(500);
+    Serial.print(".");
   }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
   server.begin();
 }
 
 void loop() {
-  digitalWrite(16, HIGH);
-  bool isSetup = preferences.getBool("isSetup", true);
-  char body[1024];
-  timeout = millis();
-  WiFiClient client = server.available();   // listen for incoming clients
-
-  byte temperature = 0;
-  byte humidity = 0;
-  byte data[40] = {0};
-  dht11.read(2, &temperature, &humidity, data);
-  String did = String(deviceid);
-  String water = String(analogRead(4));
-  String temp = String((double)temperature);
-  String humi = String((int)humidity);
-
-  strcpy(body, "{\"deviceId\":\"");
-  strcat(body, deviceid);
-  strcat(body, "\",\"water\":\"");
-  strcat(body, water.c_str());
-  strcat(body, "\",\"humidity\":\"");
-  strcat(body, humi.c_str());
-  strcat(body, "\",\"temperature\":\"");
-  strcat(body, temp.c_str());
-  strcat(body, "\"}");
-  if (!isSetup) {
-    http.begin("http://higrowapp.azurewebsites.net/api/records");
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST(body);
-
-    Serial.println(httpResponseCode);
-    Serial.println(body);
-  }
-  if (client) {                             // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:application/json");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print(body);
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    memset(linebuf, 0, sizeof(linebuf));
+    charcount = 0;
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        //read char by char HTTP request
+        linebuf[charcount] = c;
+        if (charcount < sizeof(linebuf) - 1) charcount++;
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+          float h = dht.readHumidity();
+          // Read temperature as Celsius (the default)
+          float t = dht.readTemperature();
+          // Read temperature as Fahrenheit (isFahrenheit = true)
+          float f = dht.readTemperature(true);
+          // Check if any reads failed and exit early (to try again).
+          if (isnan(h) || isnan(t) || isnan(f)) {
+            Serial.println("Failed to read from DHT sensor!");
+            strcpy(celsiusTemp, "Failed");
+            strcpy(fahrenheitTemp, "Failed");
+            strcpy(humidityTemp, "Failed");
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+          else {
+            // Computes temperature values in Celsius + Fahrenheit and Humidity
+            float hic = dht.computeHeatIndex(t, h, false);
+            dtostrf(hic, 6, 2, celsiusTemp);
+            float hif = dht.computeHeatIndex(f, h);
+            dtostrf(hif, 6, 2, fahrenheitTemp);
+            dtostrf(h, 6, 2, humidityTemp);
+            // You can delete the following Serial.print's, it's just for debugging purposes
+            /*Serial.print("Humidity: ");
+              Serial.print(h);
+              Serial.print(" %\t Temperature: ");
+              Serial.print(t);
+              Serial.print(" *C ");
+              Serial.print(f);
+              Serial.print(" *F\t Heat index: ");
+              Serial.print(hic);
+              Serial.print(" *C ");
+              Serial.print(hif);
+              Serial.print(" *F");
+              Serial.print("Humidity: ");
+              Serial.print(h);
+              Serial.print(" %\t Temperature: ");
+              Serial.print(t);
+              Serial.print(" *C ");
+              Serial.print(f);
+              Serial.print(" *F\t Heat index: ");
+              Serial.print(hic);
+              Serial.print(" *C ");
+              Serial.print(hif);
+              Serial.println(" *F");*/
+          }
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println();
+          client.println("<!DOCTYPE HTML><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+          client.println("<meta http-equiv=\"refresh\" content=\"30\"></head>");
+          client.println("<body><div style=\"font-size: 3.5rem;\"><p>ESP32 - DHT</p><p>");
+          if (atoi(celsiusTemp) >= 25) {
+            client.println("<div style=\"color: #930000;\">");
+          }
+          else if (atoi(celsiusTemp) < 25 && atoi(celsiusTemp) >= 5) {
+            client.println("<div style=\"color: #006601;\">");
+          }
+          else if (atoi(celsiusTemp) < 5) {
+            client.println("<div style=\"color: #009191;\">");
+          }
+          client.println(celsiusTemp);
+          client.println("*C</p><p>");
+          client.println(fahrenheitTemp);
+          client.println("*F</p></div><p>");
+          client.println(humidityTemp);
+          client.println("%</p><p>");
+          client.println(analogRead(32));
+          client.println("</p></div>");
+          client.println("</body></html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+          memset(linebuf, 0, sizeof(linebuf));
+          charcount = 0;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
         }
       }
     }
+    // give the web browser time to receive the data
+    delay(1);
+
     // close the connection:
     client.stop();
-    Serial.println("client disonnected");
-  }
-  if (timeout >= (60000 * 5)) {
-    preferences.putBool("isSetup", false);
-    digitalWrite(16, HIGH);
-  }
-  if (!isSetup) {
-    Serial.println("Entering sleep mode");
-    system_deep_sleep(60000000 * 10);
+    Serial.println("client disconnected");
   }
 }
